fc14737d3336436bab555c7ad7b83e3bac3110ed
mnicolas94 <miguelcok27@gmail.com>
2022-04-01 12:30:47 -0400

    FIX(build): fix mnd-utils dependency

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
             packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
             install_requires=[
                 # 'algorithms',
-                'mnd_utils >= 0.1.7'
+                'mnd_utils>=0.1.7'
                 'numpy',
                 'opencv-python',
                 'propsettings',
