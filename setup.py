99a0422b092eb948d6db907ee51be18d99056342
mnicolas94 <miguelcok27@gmail.com>
2022-04-01 11:25:09 -0400

    FIX(build): fix error on pushing tags, dont build if commit message has --dont_build in it (this commit will not be built)

--- setup.py
+++ setup.py
@@ -4,6 +4,14 @@ import subprocess
 import sys
 
 
+def should_build():
+    bash_command = "git log -1 --pretty=%B"
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    should = b"--dont_build" not in output
+    return should
+
+
 def get_last_version():
     bash_command = "git describe --tags --abbrev=0 --match v[0-9]*"
     process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
@@ -40,16 +48,16 @@ def push_new_version_as_tag(version):
 
 
 if __name__ == '__main__':
-    last_version = get_last_version()
+    build = should_build()
+    version = get_last_version()
     commit_has_tag = current_commit_has_tag()
-    if commit_has_tag:
-        version = last_version
-    else:
-        version = bump_patch(last_version)
+    if not commit_has_tag:
+        version = bump_patch(version)
 
-    if sys.argv[1] == 'pushtag' and not commit_has_tag:
-        push_new_version_as_tag(version)
-    else:
+    if sys.argv[1] == 'pushtag':
+        if not commit_has_tag:
+            push_new_version_as_tag(version)
+    elif build:
         with open("README.md", "r") as fh:
             long_description = fh.read()
 
